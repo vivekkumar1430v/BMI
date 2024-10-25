652e867fe44435b85af0185641a38761267a9b8c
vivekkumar1430v <vivekkumar1430011@gmail.com>
2024-10-25 12:54:47 +0530

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,8 @@
+height=float(input("ENTER YOUR HEIGHT IN CM:"))
+WEIGHT=float(input("ENTER YOUR WEIGHT IN KG:"))
+
+height=height/100
+
+BMI=WEIGHT/(height*height)
+
+print("YOUR BODY MASS INDEX IS :",BMI)
